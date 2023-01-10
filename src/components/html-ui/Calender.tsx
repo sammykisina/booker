54949ee2f89f9749fb196b97ba84dcfe430e0e69
sam <sammy.k.mutua@gmail.com>
2023-01-10 15:28:03 +0300

    Install A Package and Crearing The Times The Org Will Be Available For BS Hours

--- src/components/html-ui/Calender.tsx
+++ src/components/html-ui/Calender.tsx
@@ -1,9 +1,72 @@
-import React from 'react'
+import React, { useState } from "react";
+import ReactCalender from "react-calendar";
+import { add, format } from "date-fns";
+import { config } from "@/constants";
+
+type DateType = {
+  justDate: Date | null;
+  dateTime: Date | null;
+};
 
 const Calender = () => {
-  return (
-    <div>Calender</div>
-  )
-}
+  /**
+   * Components States
+   */
+  const [date, setDate] = useState<DateType>({
+    justDate: null,
+    dateTime: null,
+  });
 
-export default Calender
+  const getTimes = () => {
+    if (!date.justDate) return;
+
+    const { justDate } = date;
+    const start_time = add(justDate, { hours: config.STORE_OPENING_TIME });
+    const end_time = add(justDate, { hours: config.STORE_CLOSING_TIME });
+    // const interval = ;
+
+    const times = [];
+
+    for (
+      let index = start_time;
+      index <= end_time;
+      index = add(index, { minutes: config.BOOKING_TIME_INTERVAL })
+    ) {
+      times.push(index);
+    }
+
+    return times;
+  };
+
+  const times = getTimes();
+
+  return (
+    <div className="flex h-screen flex-col items-center justify-center">
+      {date.justDate ? (
+        <div className="flex gap-4">
+          {times?.map((time, time_index) => (
+            <div key={time_index} className="rounded-sm bg-gray-100 p-2">
+              <button
+                type="button"
+                onClick={() => setDate((prev) => ({ ...prev, dateTime: time }))}
+              >
+                {format(time, "kk:mm")}
+              </button>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <ReactCalender
+          minDate={new Date()}
+          className="REACT-CALENDER p-2"
+          view="month"
+          onClickDay={(date) =>
+            setDate((prev) => ({ ...prev, justDate: date }))
+          }
+        />
+      )}
+    </div>
+  );
+};
+
+export default Calender;
---
+++ src/constants/config.ts
@@ -0,0 +1,11 @@
+const STORE_OPENING_TIME = 9; // in hours
+const STORE_CLOSING_TIME = 12; // in hours
+const BOOKING_TIME_INTERVAL = 30; // in mins
+
+const config = {
+  STORE_OPENING_TIME,
+  STORE_CLOSING_TIME,
+  BOOKING_TIME_INTERVAL,
+};
+
+export default config;
